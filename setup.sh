eafbe987a3ba218852492085cc19486869ea981b
Mark Johnson <mark@barrenfrozenwasteland.com>
2019-01-06 18:39:16 +0000

    Added game dependencies to chroot

--- setup.sh
+++ setup.sh
@@ -4,10 +4,16 @@ if [ ! -d "dgamelaunch" ]
     then
     git clone https://github.com/paxed/dgamelaunch.git
 fi
-sudo apt install -y build-essential automake autogen autoconf make libncurses5 libncurses5-dev bison flex libsqlite3-0 libsqlite3-dev libncursesw5 libncursesw5-dev sqlite3
+# Dgamelaunch deps
+sudo apt install -y build-essential automake autogen autoconf make libncurses5 libncurses5-dev bison flex libsqlite3-0 libsqlite3-dev libncursesw5 libncursesw5-dev sqlite3 telnetd xinetd
+# Mzorpg deps
+sudo apt install dialog
 
 cd dgamelaunch
 ./autogen.sh "--with-config-file=/opt/mzorpg/etc/dgamelaunch.conf" --enable-shmem --enable-sqlite
 make
 sudo ../dgl/dgl-create-chroot
+
+cd ..
+sudo cp dgl/dgl_xinetd /etc/xinetd.d/
 
